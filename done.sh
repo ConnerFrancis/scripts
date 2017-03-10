f46af239e4977f1700d7a486538019a317f85bbc
kumo <thxbro79@gmail.com>
2017-03-10 11:08:30 -0500

    Update done.sh
    began work on done.sh

--- done.sh
+++ done.sh
@@ -7,4 +7,33 @@
 # and that would tell you the process closed and how it closed.
 
 echo $?
+
+if [ "$?" = "0" ]; then
+  notify-send 'The process finished with no errors.'
+fi
+
+if [ "$?" = "1" ]; then
+  notify-send 'The process hit a general error.'
+fi
+
+if [ "$?" = "2" ]; then
+  notify-send 'The process hit a \'misuse of shell builtins\' error.'
+fi
+
+if [ "$?" = "126" ]; then
+  notify-send 'The process hit a \'command cannot be invoked\' error.'
+fi
+
+if [ "$?" = "127" ]; then
+  notify-send 'The process hit a \'command not found\' error.'
+fi
+
+if [ "$?" = "128" ]; then
+  notify-send 'The process hit a \'invalid argument to exit\' error.'
+fi
+
+if [ "$?" = "130" ]; then
+  notify-send 'The process was manually terminate.'
+fi
+
 notify-send 'The process finished with exit code $?'
