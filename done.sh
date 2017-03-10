8ca714a7ad0e540a27bfbf2c10551fda52b51afe
ConnerFrancis <thxbro79@gmail.com>
2017-03-10 17:04:51 -0500

    fixed done.sh and junk

--- done.sh
+++ done.sh
@@ -2,26 +2,28 @@
 
 # This is a script that uses notify-send to send a notification giving how the last process closed.
 # This is particularly useful if you're doing a large command that takes a while to complete.
-# You could do 
+# You could do
 # my-command && done.sh
 # and that would tell you the process closed and how it closed.
 
-if [ $? -eq 0 ]; then
-  echo 'The process finished with no errors.'
-elif [ $? -eq 1 ]; then
-  echo 'The process hit a general error.'
-elif [ $? -eq 2 ]; then
-  echo 'The process hit a misuse of shell builtins error.'
-elif [ $? -eq 126 ]; then
-  echo 'The process hit a command cannot be invoked error.'
-elif [ $? -eq 127 ]; then
-  echo 'The process hit a command not found error.'
-elif [ $? -eq 128 ]; then
-  echo 'The process hit a invalid argument to exit error.'
-elif [ $? -gt 128 -a $? -lt 130 ]; then
-  echo 'The process hit a fatal error.'
-elif [ $? -eq 130 ]; then
-  echo 'The process was manually terminate.'
-elif [ $? -eq 255 ]; then
-  echo 'The process exited with an error, but it was out of error code range.'
-fi
+notify-send 'Process has closed/finished.'
+
+#if [ $? -eq 0 ]; then
+#  echo 'The process finished with no errors.'
+#elif [ $? -eq 1 ]; then
+#  echo 'The process hit a general error.'
+#elif [ $? -eq 2 ]; then
+#  echo 'The process hit a misuse of shell builtins error.'
+#elif [ $? -eq 126 ]; then
+#  echo 'The process hit a command cannot be invoked error.'
+#elif [ $? -eq 127 ]; then
+#  echo 'The process hit a command not found error.'
+#elif [ $? -eq 128 ]; then
+#  echo 'The process hit a invalid argument to exit error.'
+#elif [ $? -gt 128 -a $? -lt 130 ]; then
+#  echo 'The process hit a fatal error.'
+#elif [ $? -eq 130 ]; then
+#  echo 'The process was manually terminate.'
+#elif [ $? -eq 255 ]; then
+#  echo 'The process exited with an error, but it was out of error code range.'
+#fi
--- temps.sh
+++ temps.sh
@@ -3,13 +3,9 @@
 # This is a script to open my temperature monitors, nvidia-smi and lm-sensors
 # This also opens the monitor windows to the top right and bottom right corners of the screen
 
-# 'sensors' outputs the CPU temperatures
-gnome-terminal -e 'watch nvidia-smi -q -d temperature'
-
 # 'nvidia-smi' outputs the GPU temperature
 # the tool comes with nvidia gpu drivers for linux
-gnome-terminal -e 'watch sensors'
+gnome-terminal -e 'watch nvidia-smi -q -d temperature'
 
-# Now we close the original terminal
-# i'm lazy
-exit 0
+# 'sensors' outputs the CPU temperatures
+watch sensors
