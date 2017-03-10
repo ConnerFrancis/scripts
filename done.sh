df3fa0554047216336c24cff50a19164eafc0a50
kumo <thxbro79@gmail.com>
2017-03-10 11:31:30 -0500

    Update done.sh

--- done.sh
+++ done.sh
@@ -6,34 +6,20 @@
 # my-command && done.sh
 # and that would tell you the process closed and how it closed.
 
-echo $?
-
 if [ "$?" = "0" ]; then
   notify-send 'The process finished with no errors.'
-fi
-
-if [ "$?" = "1" ]; then
+elif [ "$?" = "1" ]; then
   notify-send 'The process hit a general error.'
-fi
-
-if [ "$?" = "2" ]; then
+elif [ "$?" = "2" ]; then
   notify-send 'The process hit a \'misuse of shell builtins\' error.'
-fi
-
-if [ "$?" = "126" ]; then
+elif [ "$?" = "126" ]; then
   notify-send 'The process hit a \'command cannot be invoked\' error.'
-fi
-
-if [ "$?" = "127" ]; then
+elif [ "$?" = "127" ]; then
   notify-send 'The process hit a \'command not found\' error.'
-fi
-
-if [ "$?" = "128" ]; then
+elif [ "$?" = "128" ]; then
   notify-send 'The process hit a \'invalid argument to exit\' error.'
-fi
-
-if [ "$?" = "130" ]; then
+elif [ "$?" = "130" ]; then
   notify-send 'The process was manually terminate.'
+elif [ "$?" = "255" ]; then
+  notify-send 'The process exited with an error, but it was out of error code range..'
 fi
-
-notify-send 'The process finished with exit code $?'
