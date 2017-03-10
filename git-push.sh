d595f95c0ab8af984506f9ccaadb51d74baaf522
kumo <thxbro79@gmail.com>
2017-03-10 08:06:39 -0500

    Update git-push.sh

--- git-push.sh
+++ git-push.sh
@@ -2,7 +2,7 @@
 
 # This is a script to enter in a bunch of git commands
 # I'm lazy
-# NOTE: This script assumes you want to push everything, commit everything, and push everything.
+# NOTE: This script assumes you want to add everything, commit everything, and push everything.
 #       It also assumes that you're repository is named origin and you're pushing to branch master.
 
 # first do git add .
