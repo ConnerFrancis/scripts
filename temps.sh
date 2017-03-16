20fd4a324e0face184a8734fd98a291502cfc0a8
ConnerFrancis <thxbro79@gmail.com>
2017-03-16 18:37:03 -0400

    temps.sh modified

--- temps.sh
+++ temps.sh
@@ -3,9 +3,10 @@
 # This is a script to open my temperature monitors, nvidia-smi and lm-sensors
 # This also opens the monitor windows to the top right and bottom right corners of the screen
 
+
 # 'nvidia-smi' outputs the GPU temperature
 # the tool comes with nvidia gpu drivers for linux
-gnome-terminal -e 'watch nvidia-smi -q -d temperature'
 
 # 'sensors' outputs the CPU temperatures
-watch sensors
+
+watch "sensors; nvidia-smi -q -d temperature"
